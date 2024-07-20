bae0a950e97a825ca5ae592d9d60a9e91f77f61b
Elanthirayann <elanthirayan12a@gmail.com>
2024-07-20 17:58:12 +0530

    Spidey

--- server/index.js
+++ server/index.js
@@ -1,3 +1,4 @@
+require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
@@ -8,7 +9,8 @@ const app = express();
 app.use(express.json())
 app.use(cors())
 
-
+const mongodb_uri = process.env.mongodb_uri
+const PORT = process.env.PORT || 5000
 mongoose.connect("mongodb://127.0.0.1:27017/employee")
 
 
@@ -38,6 +40,6 @@ app.post('/register', (req, res) => {
    .catch(err => res.json(err))
 })
 
-app.listen(3001, () => {
+app.listen(3001, () => { 
     console.log("Server is running on port 3001")
 })
